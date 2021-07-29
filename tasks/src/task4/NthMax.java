6041ae524869a054e7e5a1db5a12e6fa4acf40df
SivaDanthuluri <shivadanthuluri1998@gmail.com>
2021-07-29 19:31:27 +0530

    changes

---
+++ tasks/src/task4/NthMax.java
@@ -0,0 +1,29 @@
+package task4;
+
+import java.util.Arrays;
+
+public class NthMax {
+
+	public static void main(String[] args) {
+		
+		
+		int a[]= {13,45,53,56,78,78,23,14,56};
+		Arrays.sort(a);
+		
+		
+		
+		
+		
+		
+		/*int a[]= {10,25,45,2,6,9,34};
+		int max=0;
+		for(int i=0;i<a.length;i++)
+		{
+			
+		if(a[i]>=max)
+		 max =a[i];	
+		
+	}
+System.out.println(max);*/
+}
+}
---
+++ tasks/src/task7/PrimeInArray.java
@@ -0,0 +1,10 @@
+package task7;
+
+public class PrimeInArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
