bc2fef877642124584e15557c2c7ad63378f8ca8
SivaDanthuluri <shivadanthuluri1998@gmail.com>
2021-07-30 23:35:59 +0530

    updates

--- Challenges/src/challenge1/Website.java
+++ Challenges/src/challenge1/Website.java
@@ -4,20 +4,15 @@ public class Website {
 
 	public static void main(String[] args) {
 		String s="https://en.wikipedia.org/wiki/Main_Page";
-		
-		/*int start=s.indexOf("/");
-		int end=s.lastIndexOf(".");
-	//Domain name
-		System.out.println(s.substring(start+2,end+4));*/
-		
-		
-		
-		  String arr[]= s.split("/");
-		  System.out.println(arr[2]);
-			
-	
-		
-
+		if(s.substring(0,8).equals("https://"))
+		{
+			s=s.substring(8);
+		}
+		if(s.substring(0,7).equals("http://"))
+		{
+			s=s.substring(7);
+		}
+		 System.out.println(s.substring(0,s.indexOf('/')));
 	}
 	
 }
